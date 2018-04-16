1cb00963acad35654f037fcbb4f4b28da1f20a4e
Pillovvv <40228885@live.napier.ac.uk>
2018-04-16 20:58:48 +0100

    Can change resolution slightly

--- Castle-Thief/scenes/scene_options.cpp
+++ Castle-Thief/scenes/scene_options.cpp
@@ -7,11 +7,14 @@
 #include <SFML/Audio.hpp>
 using namespace std;
 using namespace sf;
+int viewX, viewY;
+
+
 void OptionsScene::Load() {
 	cout << "Options Load \n";
 	{
 
-		auto Texto= makeEntity();
+		auto Texto = makeEntity();
 		auto tee = Texto->addComponent<TextComponent>(
 			"Resolution\n1. Controls\n2.  Go Back");
 	}
@@ -22,8 +25,12 @@ void OptionsScene::Update(const double& dt) {
 
 	if (sf::Keyboard::isKeyPressed(Keyboard::Num1)) {
 		Engine::GetWindow().setSize(sf::Vector2u(1920, 1080));
+		viewX = Engine::getWindowSize().x, viewY = Engine::getWindowSize().y;
+		sf::View view1(sf::FloatRect(0, 0, viewX, viewY));
+		Engine::GetWindow().setView(view1);
+		Load();
+	
 	}
-
 	else if (sf::Keyboard::isKeyPressed(Keyboard::Num3)) {
 		Engine::ChangeScene(&menu);
 
