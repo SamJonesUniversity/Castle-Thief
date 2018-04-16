aa28faa55bd1f05da730763edf857b69165abe0c
unknown <Samljones24@gmail.com>
2018-04-16 17:32:48 +0100

    small bug fix

--- Castle-Thief/components/cmp_enemy_physics.h
+++ Castle-Thief/components/cmp_enemy_physics.h
@@ -2,24 +2,15 @@
 #include <ecm.h>
 #include "cmp_physics.h"
 
-class PlayerPhysicsComponent : public PhysicsComponent {
+class EnemyPhysicsComponent : public PhysicsComponent {
 protected:
   b2Vec2 _size;
   sf::Vector2f _maxVelocity;
-  bool _grounded;
-  float _groundspeed;
-  bool _direction;
-  bool _candash;
-  bool _canfire;
-  float _firetime;
-  float _elapsed;
-  bool _dampener;
-  bool isGrounded() const;
 
 public:
   void update(double dt) override;
 
-  explicit PlayerPhysicsComponent(Entity* p, const sf::Vector2f& size);
+  explicit EnemyPhysicsComponent(Entity* p, const sf::Vector2f& size);
 
-  PlayerPhysicsComponent() = delete;
+  EnemyPhysicsComponent() = delete;
 };
